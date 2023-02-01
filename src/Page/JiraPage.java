2df21d8ec1808a08271105b6f3df485d4b673854
Chi <kera@tripla.jp>
2023-02-01 17:06:11 +0800

    commit first

---
+++ src/Page/JiraPage.java
@@ -0,0 +1,14 @@
+package Page;
+
+import org.openqa.selenium.By;
+
+public class JiraPage {
+
+    private final By googleLoginBy = By.xpath("//*[@id=\"google-auth-button\"]/span[2]");
+
+    public void clickGoogleLogin() {
+
+    }
+
+
+}
