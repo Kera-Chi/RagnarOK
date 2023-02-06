426f5c3b6975b953078b038d1a81677a35a17644
Chi <kera@tripla.jp>
2023-02-06 09:35:10 +0800

    Run OK

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(0, 0);
+        list.add(0, 1);
+        list.add(0, 2);
+        list.add(0, 3);
+        System.out.println(list);
+
+
+//        Tmp tmp = new Tmp();
+//        int[] x = new int[5];
+//        int[] y = {53, 26, 37, 94};
+//        int[] ans = tmp.separateDigits(y);
+//        for (int i : ans) {
+//            System.out.println(i);
+//        }
+    }
+}
+
